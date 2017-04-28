0f7600e3b26c82d1c6af2d026c72b20ffe6c7679
Thony Price <thonyprice@gmail.com>
2017-04-28 13:00:28 +0200

    Sucessfully creates window

--- Window.py
+++ Window.py
@@ -7,8 +7,12 @@
 import tkinter as tk
 
 class Window(tk.Frame):
-    def __init__(self, master=None):
+    def __init__(self, board, master=None):
         tk.Frame.__init__(self, master)
-        self.grid()
+        sz = 12 
+        # self.grid(sticky=tk.N+tk.S+tk.E+tk.W)
+        self.canvas = tk.Canvas(self, width=len(board[0])*sz + 1, 
+            height=len(board)*sz+1, highlightthickness=0, bd=0, bg='grey')
+        self.quitButton = tk.Button(self, text='Quit',
+            command=self.quit)
 
-    
