114ba2fc300fc57a329c8adb21f678512cb50ed7
Niklas Lindqvist <niklasdator@n178-p221.kthopen.kth.se>
2017-04-28 11:58:16 +0200

    Intililized CellClass

---
+++ CellClass.py
@@ -0,0 +1,10 @@
+
+class CellClass:
+    def __init__(self, value, posX, posY):
+        self.value = 0
+        self.posX = posX
+        self.posY = posY
+
+    def update(self):
+
+        return
