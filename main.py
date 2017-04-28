493c2f3369bbd04b9d745620f54ce71fef931c61
Niklas Lindqvist <niklasdator@n178-p221.kthopen.kth.se>
2017-04-28 13:22:25 +0200

    Created function CreatClass and function getListNeighbourValue

--- main.py
+++ main.py
@@ -3,13 +3,50 @@
 
 # This file handles the backend part of what's displayed in Window.py
 # That is calculating which "cells" dies, lives on or dies each iteration
-# of the simulation. 
+# of the simulation.
 
 import Window
+from CellClass import CellClass
+
+
+def GetListNeighbourValue(x,y,board):
+    ValueList = []
+    for i in range(-1,2):
+        for j in range(-1,2):
+            try:
+                ValueList.append(int(board[x+i][y+j]))
+            except IndexError:
+                    ValueList.append(0)
+
+    return ValueList
+
+def creatClasses(board):
+    ListNeighbourValue = []
+    listCells = []
+    for x in range(10):
+        for y in range(10):
+            listCells.append(CellClass(int(board[x][y]), x ,y, GetListNeighbourValue(x,y,board)))
+
 
 def main():
-  win = Window.Window()
-  win.mainloop()
+    board = [
+        [0,0,0,0,0,0,0,0,0,0,0],
+        [0,1,1,0,0,0,0,0,0,0,0],
+        [0,1,1,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,1,0,0,0,0,0,0,0],
+        [0,0,0,1,0,0,1,1,1,0,0],
+        [0,0,0,1,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,1,1,0],
+        [0,0,0,0,0,0,0,0,1,1,0],
+        [0,0,0,0,0,0,0,0,0,0,0],
+    ]
+    creatClasses(board)
+    win = Window.Window()
+    win.mainloop()
+
+
 
 if __name__ == '__main__':
   main()
