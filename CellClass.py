3edb808f6d4fcbe29966e5a6691f87036826bc05
Niklas Lindqvist <niklasdator@n178-p221.kthopen.kth.se>
2017-04-28 12:32:12 +0200

    created update method

--- CellClass.py
+++ CellClass.py
@@ -1,10 +1,26 @@
 
 class CellClass:
-    def __init__(self, value, posX, posY):
+    def __init__(self, value, posX, posY, ListNeighbourValue):
         self.value = 0
         self.posX = posX
         self.posY = posY
+        self.LNV = ListNeighbourValue
 
     def update(self):
+        count = 0
+        for value in self.LNV:
+            count += value
+
+        if count == 3: #Dead cell with 3 live neighbours becomes alive/live cell with 3 stays alive
+            self.value = 1
+
+        if count == 2 and self.value = 1: #Any live cell with two live neighbours lives on to the next generation.
+            self.value = 1
+
+        else: #all other cases the cell dies
+            self.value == 0
 
         return
+
+    def getValue(self):
+        return self.value
