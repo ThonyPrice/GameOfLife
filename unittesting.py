30028644d20218ad57d7bbd13fe49a7f0d5e772b
Thony Price <thonyprice@gmail.com>
2017-05-08 13:31:29 +0200

    Renamed file from unittest to unittesting and started on test of updatecells

---
+++ unittesting.py
@@ -0,0 +1,34 @@
+# Unittesting
+
+import unittest
+import Window as win
+import CellClass as cc
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_updateCells(self):
+        test_board = [
+            [0,0,0,0,0],
+            [0,0,1,0,0],
+            [0,0,1,0,0],
+            [0,0,1,0,0],
+            [0,0,0,0,0],
+        ]
+        listOfCells = cc.createClasses(test_board)
+        new_board = updateCells(listOfCells)
+        print(new_board)
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
