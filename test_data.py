e8937d1c462924290327187492afe966adfaa52b
Thony Price <thonyprice@gmail.com>
2017-05-17 11:55:55 +0200

    Added comments and file info

--- test_data.py
+++ test_data.py
@@ -1,4 +1,9 @@
-# Test data
+#!/usr/bin/env python
+"""This file contains test boards for the file unittesting.py"""
+
+__author__ = "Thony Price, Niklas Lindqvist"
+__version__ = "1.0.1"
+__email__ = "thonyp@kth.se, nlindq@kth.se"
 
 board1 = [
     [0,0,0,0,0],
